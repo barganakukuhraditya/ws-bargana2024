6163fa2db02458930fa5415a1debf8cbcc4eb613
barganakukuhraditya <barganakukuh@gmail.com>
2024-05-31 16:04:16 +0700

    week10

--- url/url.go
+++ url/url.go
@@ -21,4 +21,5 @@ func Web(page *fiber.App) {
 	page.Get("/presensi", controller.GetPresensi)
 
 	page.Get("/presensi/:id", controller.GetPresensiID) //menampilkan data presensi berdasarkan id
+	page.Post("/insert", controller.InsertDataPresensi) //insert data presensi
 }
